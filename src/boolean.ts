b87c0ceff9c7c525a81acbe59cf286e9a13687b4
Santiago Heilborn <santiago.heilborn@gmail.com>
2025-04-05 02:25:53 -0300

    Release v0.1.0

---
+++ src/boolean.ts
@@ -0,0 +1,19 @@
+const boolean = (value: any): boolean => {
+  switch (Object.prototype.toString.call(value)) {
+    case "[object String]":
+      return ["true", "t", "yes", "y", "on", "1"].includes(
+        value.trim().toLowerCase()
+      );
+
+    case "[object Number]":
+      return value.valueOf() === 1;
+
+    case "[object Boolean]":
+      return value.valueOf();
+
+    default:
+      return false;
+  }
+};
+
+export { boolean };
